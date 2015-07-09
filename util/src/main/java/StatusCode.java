cb9fde572bfe772535fbc1d479fb2a785c08aafc
ncaq <ncaq@ncaq.net>
2015-07-09 22:07:51 +0900

    added: StatusCode(String) StatusCode.toString()

--- util/src/main/java/StatusCode.java
+++ util/src/main/java/StatusCode.java
@@ -2,20 +2,23 @@ package net.ncaq.chat.sd.util;
 
 import java.util.*;
 import java.util.concurrent.*;
-import java.util.regex.*;
 
 public class StatusCode {
     public StatusCode(final Integer code) {
         this.code = code;
     }
 
-    // public StatusCode(final String full) {
+    public StatusCode(final String full) {
+        this.code = Integer.parseInt(full.substring(0, 2));
+    }
 
-    // }
+    public String toString() {
+        return this.code.toString() + " " + message.get(this.code);
+    }
 
     private final Integer code;
 
-    private final static Map<Integer, String> codeMessage = new ConcurrentHashMap<Integer, String>(){{
+    private final static Map<Integer, String> message = new ConcurrentHashMap<Integer, String>(){{
             put(0, "login succeed");
             put(100, "password invalid");
             put(101, "multiple login");
