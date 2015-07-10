b318057aad439a61ca0345f43c97d4b9b3e55b47
ncaq <ncaq@ncaq.net>
2015-07-10 10:50:30 +0900

    added: javadoc comment to class User

--- util/src/main/java/User.java
+++ util/src/main/java/User.java
@@ -3,11 +3,18 @@ package net.ncaq.chat.sd.util;
 import java.net.*;
 import java.security.*;
 
+/**
+ * ユーザー情報保存クラス
+ * 一応パスワードをハッシュ化するが,
+ * 今後セキュリティ強化を可能な設計にすることが目的なので,
+ * 現在はSHA-256を1回行うだけの全く無意味なものです.
+ * 実用性を考えるならセキュリティ関連は自分で書きません.
+ */
 public class User {
     public User(final String username, final String rawPassword) {
         String passwordStash = null;
         try {
-            final MessageDigest md = MessageDigest.getInstance("SHA-256"); // やらないよりはマシレベル
+            final MessageDigest md = MessageDigest.getInstance("SHA-256");
             md.update(rawPassword.getBytes());
             passwordStash = md.toString();
         }
