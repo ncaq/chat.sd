a62ececb26e1bb2c08bed85aea90f17e4baaff28
ncaq <ncaq@ncaq.net>
2015-07-11 21:09:51 +0900

    added: anonymousLoginIsSuccess

--- src/test/java/ConnectTest.java
+++ src/test/java/ConnectTest.java
@@ -4,23 +4,16 @@ import java.io.*;
 import java.net.*;
 import net.ncaq.chat.sd.client.*;
 import net.ncaq.chat.sd.server.*;
+import net.ncaq.chat.sd.util.*;
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class ConnectTest {
     @Test
-    public void testStartSession() throws IOException, UnknownHostException {
-        InputStream stdin = System.in;
-        try {
-            new ChatServer(50000);
-
-            final PipedOutputStream pipe = new PipedOutputStream();
-            System.setIn(new PipedInputStream(pipe));
-            final PrintWriter pin = new PrintWriter(new BufferedOutputStream(pipe));
-
-            new ConsoleClient("localhost", "anonymous", "");
-        } finally {
-            System.setIn(stdin);
-        }
+    public void anonymousLoginIsSuccess() throws Exception {
+        new ChatServer(50000);
+        final User u = new User("anonymous", "");
+        final Connector c = new Connector("localhost", u);
+        assertEquals(c.readLine(), new StatusCode(0).toString());
     }
 }
