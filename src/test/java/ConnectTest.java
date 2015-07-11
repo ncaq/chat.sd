0253ee63f09c30d0b5b79dbae6f5d0f9c5bdd88c
ncaq <ncaq@ncaq.net>
2015-07-11 20:18:08 +0900

    moved: join test move to root directory

--- src/test/java/ConnectTest.java
+++ src/test/java/ConnectTest.java
@@ -1,14 +1,15 @@
-package net.ncaq.chat.sd.client;
+package net.ncaq.chat.sd;
 
 import java.io.*;
 import java.net.*;
+import net.ncaq.chat.sd.client.*;
 import net.ncaq.chat.sd.server.*;
 import org.junit.*;
 import static org.junit.Assert.*;
 
-public class ConsoleClientTest {
+public class ConnectTest {
     @Test
-    public void testConsoleClient() throws IOException, UnknownHostException {
+    public void testStartSession() throws IOException, UnknownHostException {
         InputStream stdin = System.in;
         try {
             new ChatServer(50000);
