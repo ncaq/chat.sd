caeaea04c27c82f181199ce8166ba66f873e1033
ncaq <ncaq@ncaq.net>
2015-07-14 13:29:12 +0900

    fixed: nothing argument pattern

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -5,15 +5,19 @@ import gnu.getopt.Getopt;
 public class Main {
     public static void main(final String[] args) throws Exception {
         final Getopt opts = new Getopt("chat.sd.client", args, "c");
+        Boolean console = false;
         for(int o = opts.getopt(); o != -1; o = opts.getopt()) {
             switch(o) {
             case 'c':
-                new ConsoleClient();
-                break;
-            default:
-                javafx.application.Application.launch(JavaFxClient.class, args);
+                console = true;
                 break;
             }
+        }
+        if(console) {
+            new ConsoleClient();
+        }
+        else {
+            javafx.application.Application.launch(JavaFxClient.class, args);
         }
     }
 }
