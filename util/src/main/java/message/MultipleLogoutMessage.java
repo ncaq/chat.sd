d1eb0fd2e275e9fbfbf813f65284dca6f5c83592
ncaq <ncaq@ncaq.net>
2015-07-16 08:25:06 +0900

    added: MultipleLogoutMessage

---
+++ util/src/main/java/message/MultipleLogoutMessage.java
@@ -0,0 +1,27 @@
+package net.ncaq.chat.sd.util.message;
+
+import java.sql.*;
+import javax.persistence.*;
+import lombok.*;
+
+/**
+ * 多重ログアウト
+ */
+@Data
+@Entity
+public class MultipleLogoutMessage extends Message {
+    @Override
+    public String type() {
+        return "multiple logout";
+    }
+
+    @Override
+    public Integer code() {
+        return 601;
+    }
+
+    @Override
+    public String description() {
+        return "(extension)";
+    }
+}
