eebdcb5229d8ff0357372a471c99a8201f5ed5e3
ncaq <ncaq@ncaq.net>
2015-07-15 13:07:19 +0900

    changed: AuthTest for enum Status

--- server/src/test/java/AuthTest.java
+++ server/src/test/java/AuthTest.java
@@ -8,6 +8,7 @@ import java.util.stream.*;
 import net.ncaq.chat.sd.util.*;
 import org.junit.*;
 import static junit.framework.Assert.*;
+import static net.ncaq.chat.sd.util.Status.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
@@ -16,18 +17,18 @@ public class AuthTest {
     public void existUserIs0() throws Exception {
         final Auth a = new Auth();
         a.addUser(new User("sampleUser", "pass"));
-        assertThat(a.login(new User("sampleUser", "pass")), is(new StatusCode(0)));
+        assertThat(a.login(new User("sampleUser", "pass")), is(LOGIN_SUCCEED));
     }
 
     @Test
     public void anonymousLogin() throws Exception {
-        assertThat(new Auth().login(new User("anonymous", "")), is(new StatusCode(0)));
+        assertThat(new Auth().login(new User("anonymous", "")), is(LOGIN_SUCCEED));
     }
 
     @Test
     public void nothingUserIs100() throws Exception {
         final Auth a = new Auth();
-        assertThat(a.login(new User("sampleUser", "pass")), is(new StatusCode(100)));
+        assertThat(a.login(new User("sampleUser", "pass")), is(PASSWORD_INVALID));
     }
 
     @Test
@@ -40,9 +41,9 @@ public class AuthTest {
     public void loginAndLogoutAndLogin() {
         final Auth a = new Auth();
         a.addUser(new User("sampleUser", "pass"));
-        assertThat(a.login(new User("sampleUser", "pass")), is(new StatusCode(0)));
-        assertThat(a.logout(new User("sampleUser", "pass")), is(new StatusCode(600)));
-        assertThat(a.login(new User("sampleUser", "pass")), is(new StatusCode(0)));
+        assertThat(a.login(new User("sampleUser", "pass")), is(LOGIN_SUCCEED));
+        assertThat(a.logout(new User("sampleUser", "pass")), is(LOGOUT_SUCCEED));
+        assertThat(a.login(new User("sampleUser", "pass")), is(LOGIN_SUCCEED));
     }
 
     @Test
@@ -55,13 +56,13 @@ public class AuthTest {
 
         final ExecutorService lio = Executors.newFixedThreadPool(2 * threads + 1);
 
-        Collection<Callable<StatusCode>> cl = new ConcurrentLinkedQueue<>();
+        Collection<Callable<Status>> cl = new ConcurrentLinkedQueue<>();
         for(int i = 0; i < threads; ++i) {
             cl.add(() -> a.login(u));
             cl.add(() -> a.logout(u));
         }
 
-        List<Future<StatusCode>> fsl = lio.invokeAll(cl);
+        List<Future<Status>> fsl = lio.invokeAll(cl);
         fsl.forEach(f -> {
                 try {
                     f.get();
@@ -72,7 +73,7 @@ public class AuthTest {
             }); // wait
         fsl.add(lio.submit(() -> a.logout(u)));
 
-        final List<StatusCode> sl = fsl.stream().map(f -> {
+        final List<Status> sl = fsl.stream().map(f -> {
                 try {
                     return f.get();
                 }
@@ -83,8 +84,8 @@ public class AuthTest {
             }).collect(Collectors.toList());
 
         assertThat("status list: " + Arrays.deepToString(sl.toArray()),
-                   sl.stream().filter(s -> s.equals(new StatusCode(0))).count() -
-                   sl.stream().filter(s -> s.equals(new StatusCode(600))).count(),
+                   sl.stream().filter(s -> s.equals(LOGIN_SUCCEED)).count() -
+                   sl.stream().filter(s -> s.equals(LOGOUT_SUCCEED)).count(),
                    is(0L));
     }
 }
