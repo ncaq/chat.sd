91d2ed65bc872017d514a481f923f44d0db0b737
ncaq <ncaq@ncaq.net>
2015-07-15 13:05:07 +0900

    changed: testcase for enum Status

--- util/src/test/java/StatusTest.java
+++ util/src/test/java/StatusTest.java
@@ -1,27 +1,32 @@
 package net.ncaq.chat.sd.util;
 
 import org.junit.*;
+import static net.ncaq.chat.sd.util.Status.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
 public class StatusTest {
     @Test
-    public void testStatusInteger() {
-        assertThat(new Status(0).toString(), is("0 login succeed"));
-        assertThat(new Status(100).toString(), is("100 password invalid"));
-        assertThat(new Status(101).toString(), is("101 multiple login"));
+    public void ToString() {
+        assertThat(LOGIN_SUCCEED   .toString(), is(  "0 login succeed"));
+        assertThat(PASSWORD_INVALID.toString(), is("100 password invalid"));
+        assertThat(MULTIPLE_LOGIN  .toString(), is("101 multiple login"));
+        assertThat(LOGOUT_SUCCEED  .toString(), is("600 logout succeed(extension)"));
+        assertThat(MULTIPLE_LOGOUT .toString(), is("601 multiple logout(extension)"));
     }
 
     @Test
-    public void testStatusString() {
-        assertThat(new Status("0 login succeed").getCode(), is(new Integer(0)));
-        assertThat(new Status("100 login succeed").getCode(), is(new Integer(100)));
-        assertThat(new Status("101 login succeed").getCode(), is(new Integer(101)));
+    public void fromFullMessage() {
+        assertThat("  0 login succeed"   , is(LOGIN_SUCCEED));
+        assertThat("100 password invalid", is(PASSWORD_INVALID));
+        assertThat("101 multiple login"  , is(MULTIPLE_LOGIN));
+        assertThat("600 logout succeed"  , is(LOGOUT_SUCCEED));
+        assertThat("601 multiple logout" , is(MULTIPLE_LOGOUT));
     }
 
     @Test
-    public void testEquals() {
-        assertThat(new Status(0), is(new Status(0)));
-        assertThat(new Status(0), equalTo(new Status(0)));
+    public void Equals() {
+        assertThat(LOGIN_SUCCEED, is(LOGIN_SUCCEED));
+        assertThat(LOGIN_SUCCEED, is(not(PASSWORD_INVALID)));
     }
 }
