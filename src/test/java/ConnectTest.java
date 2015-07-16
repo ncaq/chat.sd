42eeea19951d67e60604de865b5e71853e1a1836
ncaq <ncaq@ncaq.net>
2015-07-16 18:53:36 +0900

    added: Whitebox test

--- src/test/java/ConnectTest.java
+++ src/test/java/ConnectTest.java
@@ -4,17 +4,21 @@ import java.io.*;
 import java.net.*;
 import java.util.concurrent.*;
 import lombok.*;
+import net.ncaq.chat.sd.*;
 import net.ncaq.chat.sd.client.*;
 import net.ncaq.chat.sd.server.*;
-import net.ncaq.chat.sd.*;
 import org.junit.*;
+import org.mockito.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
 public class ConnectTest {
     @Test
     public void anonymousLoginIsSuccess() throws Exception {
-        new CentralServer(50000);
+        val server = new CentralServer(50000);
+        val auth = new Auth();
+        auth.addUser(new User("anonymous", ""));
+        Whitebox.setInternalState(server, "auth", auth);
         val q = new LinkedBlockingQueue<String>();
         val c = new Connector("localhost", "anonymous", "", (m) -> q.add(m));
         assertThat(q.take(), is(notNullValue()));
