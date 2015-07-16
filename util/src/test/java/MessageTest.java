81a595515bf039e25bb4c6f367b609126399ffe1
ncaq <ncaq@ncaq.net>
2015-07-16 15:24:32 +0900

    added: test of ChatMessage and LoginMessage

--- util/src/test/java/MessageTest.java
+++ util/src/test/java/MessageTest.java
@@ -10,4 +10,25 @@ public class MessageTest {
     public void fromCode() throws Exception {
         assertThat(Message.fromCode(0), is(instanceOf(LoginMessage.class)));
     }
+
+    @Test
+    public void ChatMessage() throws Exception {
+        final Message cm = Message.fromCode(200);
+        cm.setPoster(new User("hoge", "huga"));
+        cm.setBody("foo");
+        assertThat(cm.type(), is("chat"));
+        assertThat(cm.code(), is(200));
+        assertThat(cm.description(), is("ok(extension)"));
+        assertThat(cm.forTimeLine(), startsWith("chat hoge foo"));
+    }
+
+    @Test
+    public void LoginMessage() throws Exception {
+        final Message lm = Message.fromCode(0);
+        lm.setPoster(new User("hoge", "huga"));
+        assertThat(lm.type(), is("login"));
+        assertThat(lm.code(), is(0));
+        assertThat(lm.description(), is("succeed"));
+        assertThat(lm.forTimeLine(), startsWith("login user hoge"));
+    }
 }
