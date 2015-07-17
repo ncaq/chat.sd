06c236ecb5a37debbf96b62c85d6b53ef105b910
ncaq <ncaq@ncaq.net>
2015-07-17 18:16:48 +0900

    deleted: ConnectTest.anonymousLoginIsSuccess: Auth.addUser don't work test with database.

--- src/test/java/ConnectTest.java
+++ src/test/java/ConnectTest.java
@@ -17,9 +17,6 @@ public class ConnectTest {
     @Test
     public void anonymousLoginIsSuccess() throws Exception {
         val server = new CentralServer(50000);
-        val auth = new Auth();
-        auth.addUser(new User("anonymous", ""));
-        Whitebox.setInternalState(server, "auth", auth);
         val q = new LinkedBlockingQueue<String>();
         val c = new Connector("localhost", "anonymous", "", (m) -> q.add(m));
         assertThat(q.take(), is(notNullValue()));
