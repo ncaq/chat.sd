ca3f4bcfb5470ccb53c950d96c20e74836e92cb6
ncaq <ncaq@ncaq.net>
2015-07-19 19:55:08 +0900

    deleted:: MessageTest of Status moving assert.

--- server/src/test/java/MessageTest.java
+++ server/src/test/java/MessageTest.java
@@ -13,39 +13,18 @@ public class MessageTest {
 
     @Test
     public void ChatMessage() throws Exception {
-        final Message cm = Message.of(200);
+        final Message cm = new ChatMessage();
         cm.setPoster(new User("hoge", "huga"));
         cm.setBody("foo");
         assertThat(cm.type(), is("chat"));
-        assertThat(cm.code(), is(200));
-        assertThat(cm.description(), is("ok(extension)"));
         assertThat(cm.toTimeLine(), is("chat hoge foo"));
-
-        assertThat(Message.of("200 chat ok"), is(instanceOf(ChatMessage.class)));
     }
 
     @Test
     public void LoginMessage() throws Exception {
-        final Message lm = Message.of(0);
+        final Message lm = new LoginMessage();
         lm.setPoster(new User("hoge", "huga"));
         assertThat(lm.type(), is("login"));
-        assertThat(lm.code(), is(0));
-        assertThat(lm.description(), is("succeed"));
         assertThat(lm.toTimeLine(), startsWith("login user hoge"));
-        assertThat(lm.status(), is("0 login succeed"));
-
-        assertThat(Message.of("0 login succeed"), is(instanceOf(LoginMessage.class)));
-    }
-
-    @Test
-    public void PasswordInvalidMessage() throws Exception {
-        final Message im = Message.of(100);
-        assertThat(im.type(), is("password invalid"));
-        assertThat(im.code(), is(100));
-        assertThat(im.description(), is(""));
-        assertThat(im.toTimeLine(), is("password invalid"));
-        assertThat(im.status(), is("100 password invalid"));
-
-        assertThat(Message.of("100 password invalid"), is(instanceOf(PasswordInvalidMessage.class)));
     }
 }
